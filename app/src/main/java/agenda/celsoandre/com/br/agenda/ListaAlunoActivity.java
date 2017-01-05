c198bfe596459572c091da53119f02328110d031
Celso Andre <celsolog2012@gmail.com>
2017-01-05 08:17:12 -0200

    Implementando ListView | ArrayAdapter

--- app/src/main/java/agenda/celsoandre/com/br/agenda/ListaAlunoActivity.java
+++ app/src/main/java/agenda/celsoandre/com/br/agenda/ListaAlunoActivity.java
@@ -2,12 +2,25 @@ package agenda.celsoandre.com.br.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunoActivity extends AppCompatActivity {
-
+    private ListView list_item;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+        String[] alunos = {"celso", "andre", "renan", "caio"};
+        //Vinculando Views
+        list_item = (ListView) findViewById(R.id.lista_alunos);
+        //Criando Adapter
+        adapter = new ArrayAdapter<String>(this,     //contexto
+                android.R.layout.simple_list_item_1, //Layout
+                alunos);                             //Array
+        //Setando o Adapter na listView
+        list_item.setAdapter(adapter);
+
     }
 }
